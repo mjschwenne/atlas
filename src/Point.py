ee9aca4cfbcc1f5cfd656146c155f4527e7a34b5
hexedwalnut <jteahe02@gmail.com>
2021-02-12 16:38:24 -0500

    Made Point.py a class, and added a docstring for Point.py.

--- src/Point.py
+++ src/Point.py
@@ -1,87 +1,105 @@
 import math
 
-x = 0  # The point's x value
-y = 0  # The point's y value
 
-
-def __init__(self, x_init, y_init):
+class Point:
     """
-    Point Constructor
+    Point with associated distance functions
 
-    Parameters
+    Attributes
     ----------
-    self : Point
-        The point being constructed
-    x_init : float
-        The initial x value of this Point
-    y_init : float
-        The initial x value of this Point
-    """
-    self.x = x_init
-    self.y = y_init
+    x : float
+        The point's x value
+    y : float
+        The point's y value
 
-
-def set_x(self, x_new):
-    """
-    Sets the value of x for a point
-
-    Parameters
-    ----------
-    self : Point
-        The point
-    x_new
-        The new x value of the point
-    """
-    self.x = x_new
-
-
-def set_y(self, y_new):
-    """
-    Sets the value of y for a point
-
-    Parameters
-    ----------
-    self : Point
-        The point
-    y_new
-        The new y value of the point
-    """
-    self.y = y_new
-
-
-def simple_distance(self, point):
-    """
-    Finds the distance between this point and another point
-
-    Parameters
-    ----------
-    self : Point
-        The initial point
-    point : Point
-        The point to find the distance between
-
-    Returns
+    Methods
     -------
-    float
-        The simple distance between this point and another point
+    set_x(x_new)
+        Sets the x value of the point
+    set_y(y_new)
+        Sets the y value of the point
+    simple_distance(point)
+        Finds the simple distance between this point and another point
+    manhattan_distance(point)
+        Finds the manhattan distance between this point and another point
     """
-    return math.sqrt(math.pow((point.x - self.x), 2) + math.pow((point.y - self.y), 2))
+    x = 0  # The point's x value
+    y = 0  # The point's y value
 
+    def __init__(self, x_init, y_init):
+        """
+        Point Constructor
 
-def manhattan_distance(self, point):
-    """
-    Finds the manhattan distance between this point and another point
+        Parameters
+        ----------
+        self : Point
+            The point being constructed
+        x_init : float
+            The initial x value of this Point
+        y_init : float
+            The initial x value of this Point
+        """
+        self.x = x_init
+        self.y = y_init
 
-    Parameters
-    ----------
-    self : Point
-        The initial point
-    point : Point
-        The point to find the distance between
+    def set_x(self, x_new):
+        """
+        Sets the value of x for a point
 
-    Returns
-    -------
-    float
-        The manhattan distance between this point and another point
-    """
-    return abs(self.x - point.x) + abs(self.y - point.y)
+        Parameters
+        ----------
+        self : Point
+            The point
+        x_new
+            The new x value of the point
+        """
+        self.x = x_new
+
+    def set_y(self, y_new):
+        """
+        Sets the value of y for a point
+
+        Parameters
+        ----------
+        self : Point
+            The point
+        y_new
+            The new y value of the point
+        """
+        self.y = y_new
+
+    def simple_distance(self, point):
+        """
+        Finds the distance between this point and another point
+
+        Parameters
+        ----------
+        self : Point
+            The initial point
+        point : Point
+            The point to find the distance between
+
+        Returns
+        -------
+        float
+            The simple distance between this point and another point
+        """
+        return math.sqrt(math.pow((point.x - self.x), 2) + math.pow((point.y - self.y), 2))
+
+    def manhattan_distance(self, point):
+        """
+        Finds the manhattan distance between this point and another point
+
+        Parameters
+        ----------
+        self : Point
+            The initial point
+        point : Point
+            The point to find the distance between
+
+        Returns
+        -------
+        float
+            The manhattan distance between this point and another point
+        """
+        return abs(self.x - point.x) + abs(self.y - point.y)
