70e232d88a22cea62bf7b4ffbadd82a077ae8e1b
mjschwenne <mjschwenne@gmail.com>
2021-02-12 18:25:25 -0500

    updated docstrings and created the documentation files.

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Atlas documentation master file, created by
+   sphinx-quickstart on Fri Feb 12 17:56:05 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Atlas's documentation!
+=================================
+Documentation is updated on Wednesdays and Fridays.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   main
+   Point
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/Point.py
+++ src/Point.py
@@ -3,103 +3,116 @@ import math
 
 class Point:
     """
-    Point with associated distance functions
+    Point storing x and y coordinates with associated distance functions.
 
     Attributes
     ----------
     x : float
-        The point's x value
+        The point's x value.
     y : float
-        The point's y value
+        The point's y value.
 
     Methods
     -------
     set_x(x_new)
-        Sets the x value of the point
+        Sets the x value of the point.
     set_y(y_new)
-        Sets the y value of the point
+        Sets the y value of the point.
     simple_distance(point)
-        Finds the simple distance between this point and another point
+        Finds the simple distance between this point and another point.
     manhattan_distance(point)
-        Finds the manhattan distance between this point and another point
+        Finds the manhattan distance between this point and another point.
     """
-    x = 0  # The point's x value
-    y = 0  # The point's y value
 
-    def __init__(self, x_init, y_init):
+    def __init__(self, x, y):
         """
         Point Constructor
 
         Parameters
         ----------
-        self : Point
-            The point being constructed
-        x_init : float
-            The initial x value of this Point
-        y_init : float
-            The initial x value of this Point
+        x : float
+            The initial x value of this Point.
+        y : float
+            The initial x value of this Point.
         """
-        self.x = x_init
-        self.y = y_init
+        self.x = x
+        self.y = y
 
     def set_x(self, x_new):
         """
-        Sets the value of x for a point
+        Sets the value of `x` for a point.
 
         Parameters
         ----------
-        self : Point
-            The point
-        x_new
-            The new x value of the point
+        x_new : float
+            The new `x` value of the point.
         """
         self.x = x_new
 
+    def get_x(self):
+        """
+        Fetch the value of `x`.
+
+        Returns
+        -------
+        float:
+            `x`-coordinate of this Point.
+        """
+        return self.x
+
     def set_y(self, y_new):
         """
-        Sets the value of y for a point
+        Sets the value of `y` for a point.
 
         Parameters
         ----------
-        self : Point
-            The point
         y_new
-            The new y value of the point
+            The new `y` value of the point.
         """
         self.y = y_new
 
+    def get_y(self):
+        """
+        Fetch the value of `y`.
+
+        Returns
+        -------
+        float:
+            `y`-coordinate of this Point.
+        """
+        return self.y
+
     def simple_distance(self, point):
         """
-        Finds the distance between this point and another point
+        Finds the distance between this point and another point.
+
+        .. math:: d = \sqrt{(x_2 - x_1) + (y_2 - y_1)}
 
         Parameters
         ----------
-        self : Point
-            The initial point
         point : Point
-            The point to find the distance between
+            The point to find the distance between.
 
         Returns
         -------
         float
-            The simple distance between this point and another point
+            The simple distance between this point and another point.
         """
         return math.sqrt(math.pow((point.x - self.x), 2) + math.pow((point.y - self.y), 2))
 
     def manhattan_distance(self, point):
         """
-        Finds the manhattan distance between this point and another point
+        Finds the manhattan distance between this point and another point,
+        which is the total displacement in x and y.
 
         Parameters
         ----------
-        self : Point
-            The initial point
         point : Point
-            The point to find the distance between
+            The point to find the distance between.
 
         Returns
         -------
         float
-            The manhattan distance between this point and another point
+            The manhattan distance between this point and another point.
         """
         return abs(self.x - point.x) + abs(self.y - point.y)
