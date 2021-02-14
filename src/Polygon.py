570676ab3ea57d26ddc53601ff78cf27902848d3
hexedwalnut <jteahe02@gmail.com>
2021-02-14 18:56:44 -0500

    Wrote some more tests for Polygon.py in PolygonTests.py, and implemented is_contained method in Polygon.py.

--- src/Polygon.py
+++ src/Polygon.py
@@ -1,4 +1,106 @@
 import math
+from src.Point import Point
+
+
+def _three_point_orientation(p1, p2, p3):
+    """
+    Returns the orientation of three points in the form of an integer
+
+    Parameters
+    ----------
+    p1 : Point
+        First point in the three point orientation
+    p2 : Point
+        Second point in the three point orientation
+    p3 : Point
+        Third point in the three point orientation
+
+    Returns
+    -------
+    int
+        collinear = 0
+        clockwise = 1
+        counter-clockwise = 2
+    """
+    slope_p1_p2 = (p2.get_y() - p1.get_y()) / (p2.get_x() - p1.get_x())
+    slope_p2_p3 = (p3.get_y() - p2.get_y()) / (p3.get_x() - p2.get_x())
+
+    # If slope_p1_p2 == slope_p2_p3 then the three points are collinear
+    if slope_p1_p2 == slope_p2_p3:
+        return 0
+    # If slope_p1_p2 > slope_p2_p3 then the three points are clockwise (right turn)
+    elif slope_p1_p2 > slope_p2_p3:
+        return 1
+    # If slope_p1_p2 < slope_p2_p3 then the three points are counter-clockwise (left turn)
+    else:
+        return 2
+
+
+def _in_segment(p1, p2, p3):
+    """
+    Returns true if p3 is in the line segment from p1 to p2.
+
+    Parameters
+    ----------
+    p1 : Point
+        First point of the line segment
+    p2 : Point
+        Second point of the line segment
+    p3 : Point
+        The point to see if it is in the line segment
+
+    Returns
+    -------
+    bool
+        True if p3 is in the line segment from p1 to p2
+    """
+    if (min(p1.get_x(), p2.get_x()) <= p3.get_x() <= max(p1.get_x(), p2.get_x()) and
+            min(p1.get_y(), p2.get_y()) <= p3.get_y() <= max(p1.get_y(), p2.get_y())):
+        return True
+    return False
+
+
+def _intersects(p1, q1, p2, q2):
+    """
+    Returns true if line segment from p1 to q1 intersects line segment from p2 to q2
+
+    Parameters
+    ----------
+    p1 : Point
+        The first point in the first line segment
+    q1 : Point
+        The second point in the first line segment
+    p2 : Point
+        The first point in the second line segment
+    q2 : Point
+        The second point in the second line segment
+
+    Returns
+    -------
+    bool
+        True if the line segment from p1 to q1 intersects line segment from p2 to q2
+    """
+    p1_q1_p2_or = _three_point_orientation(p1, q1, p2)  # Orientation between p1, q1, p2
+    p1_q1_q2_or = _three_point_orientation(p1, q1, q2)  # Orientation between p1, q1, p2
+
+    p2_q2_p1_or = _three_point_orientation(p2, q2, p1)  # Orientation between p2, q2, p1
+    p2_q2_q1_or = _three_point_orientation(p2, q2, q1)  # Orientation between p2, q2, q1
+
+    # General case
+    if p1_q1_p2_or != p1_q1_q2_or and p2_q2_p1_or != p2_q2_q1_or:
+        return True
+
+    # Special cases
+    if p1_q1_p2_or == 0 and _in_segment(p1, q1, p2):
+        return True
+    if p1_q1_q2_or == 0 and _in_segment(p1, q1, q2):
+        return True
+    if p2_q2_p1_or == 0 and _in_segment(p2, q2, p1):
+        return True
+    if p2_q2_q1_or == 0 and _in_segment(p2, q2, q1):
+        return True
+
+    return False
 
 
 class Polygon:
@@ -9,7 +111,7 @@ class Polygon:
 
         Parameters
         ----------
-        vertices : List of Points
+        vertices : List of Point
             The vertices that define the boundaries of the Polygon. The vertices must be ordered clockwise.
         """
         self.vertices = vertices
@@ -113,7 +215,36 @@ class Polygon:
         return round(abs(angle_sum), 5) == round(math.pi * 2, 5)
 
     def is_contained(self, point):
-        pass
+        """
+
+        Parameters
+        ----------
+        point : Point
+            The point to check if in this polygon
+
+        Returns
+        -------
+        bool
+            True if contained in the polygon
+        """
+        n = len(self.vertices)
+
+        # If n < 3, it can't be closed, thus no point is contained
+        if n < 3:
+            return False
+
+        max_x = max(self.vertices.get_x)
+        ext = Point(max_x, point.get_y())
+        count = 0
+
+        for i in range(n - 1):
+            cur_point = self.vertices[i]
+            next_point = self.vertices[i + 1]
+
+            if _intersects(cur_point, next_point, point, ext):
+                count += 1
+
+        return (count % 2) == 1
 
     def is_bordering(self):
         pass
--- test/PolygonTests.py
+++ test/PolygonTests.py
@@ -1,36 +1,60 @@
 import unittest
-from src import Polygon
-from src import Point
+from src.Polygon import Polygon
+from src.Point import Point
 
 
-class TestPolygonSquares(unittest.TestCase):
+class TestPolygon(unittest.TestCase):
     # Square 1
     # ========
-    s1p1 = Point.Point(0.0, 0.0)
-    s1p2 = Point.Point(0.0, 1.0)
-    s1p3 = Point.Point(1.0, 1.0)
-    s1p4 = Point.Point(1.0, 0.0)
+    s1p1 = Point(0.0, 0.0)
+    s1p2 = Point(0.0, 1.0)
+    s1p3 = Point(1.0, 1.0)
+    s1p4 = Point(1.0, 0.0)
     s1ver = [s1p1, s1p2, s1p3, s1p4]
 
     # Square 2
     # ========
-    s2p1 = Point.Point(0.0, 0.0)
-    s2p2 = Point.Point(0.0, 10.0)
-    s2p3 = Point.Point(10.0, 10.0)
-    s2p4 = Point.Point(10.0, 0.0)
+    s2p1 = Point(0.0, 0.0)
+    s2p2 = Point(0.0, 10.0)
+    s2p3 = Point(10.0, 10.0)
+    s2p4 = Point(10.0, 0.0)
     s2ver = [s2p1, s2p2, s2p3, s2p4]
+
+    # Less than 3 points
+    # ==================
+    sh1p1 = Point(0.0, 0.0)
+    sh1p2 = Point(0.0, 0.1)
+    sh1ver = [sh1p1, sh1p2]
+
+    # Repeated point
+    # ==============
+    sh2p1 = Point(0.0, 0.0)
+    sh2p2 = Point(0.0, 0.0)
+    sh2p3 = Point(0.0, 1.0)
+    sh2p4 = Point(1.0, 1.0)
+    sh2p5 = Point(1.0, 0.0)
+    sh2ver = [sh2p1, sh2p2, sh2p3, sh2p4, sh2p5]
+
+    # Pentagram (intersecting) Shape
+    # ==============================
+    sh3p1 = Point(0.0, 5.0)
+    sh3p2 = Point(4.0, -5.0)
+    sh3p3 = Point(-5.0, 2.0)
+    sh3p4 = Point(5.0, 2.0)
+    sh3p5 = Point(-4.0, -5.0)
+    sh3ver = [sh3p1, sh3p2, sh3p3, sh3p4, sh3p5]
 
     # Square 1 Tests
     def test_perimeter_s1(self):
         """
         Test of the Polygon.py get_perimeter method for s1
         """
-        poly = Polygon.Polygon(self.s1ver)
+        poly = Polygon(self.s1ver)
 
         self.assertEqual(4, poly.get_perimeter())
 
     def test_is_convex_s1(self):
-        poly = Polygon.Polygon(self.s1ver)
+        poly = Polygon(self.s1ver)
 
         self.assertEqual(True, poly.is_convex())
 
@@ -39,7 +63,7 @@ class TestPolygonSquares(unittest.TestCase):
         """
         Test of the Polygon.py get_perimeter method for s2
         """
-        poly = Polygon.Polygon(self.s2ver)
+        poly = Polygon(self.s2ver)
 
         self.assertEqual(40, poly.get_perimeter())
 
@@ -47,9 +71,35 @@ class TestPolygonSquares(unittest.TestCase):
         """
         Test of the Polygon.py is_convex method for s2
         """
-        poly = Polygon.Polygon(self.s2ver)
+        poly = Polygon(self.s2ver)
 
         self.assertEqual(True, poly.is_convex())
+
+    # is_convex method tests
+    def test_less_than_three_points_is_convex(self):
+        """
+        Test of a Polygon with less than 3 points (should return false for is_convex)
+        """
+        poly = Polygon(self.sh1ver)
+
+        self.assertEqual(False, poly.is_convex())
+
+    def test_repeated_point_is_convex(self):
+        """
+        Test of a Polygon with a repeated point (should return false for is_convex)
+        """
+        poly = Polygon(self.sh2ver)
+
+        self.assertEqual(False, poly.is_convex())
+
+    def test_pentagram_is_convex(self):
+        """
+        Test of a Polygon with intersecting lines, a pentagram (should return false for is_convex)
+        """
+        poly = Polygon(self.sh3ver)
+
+        self.assertEqual(False, poly.is_convex())
+
 
 if __name__ == '__main__':
     unittest.main()
